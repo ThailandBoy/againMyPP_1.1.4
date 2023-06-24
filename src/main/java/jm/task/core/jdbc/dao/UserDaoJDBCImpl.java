0e5e2a73ac6e9d9a6fa151adacc4aa3b44fea743
ThailandBoy <91521829+ThailandBoy@users.noreply.github.com>
2023-06-24 08:02:55 +0300

    Seems fix is done...

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,63 +3,63 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    private static Connection con;
-    private final String CREATEUSERTABLE = "CREATE TABLE IF NOT EXISTS `userdb`.`user` (\n" +
-            "  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,\n" +
-            "  `name` VARCHAR(45) NULL,\n" +
-            "  `lastName` VARCHAR(45) NULL,\n" +
-            "  `age` TINYINT(3) NULL,\n" +
-            "  PRIMARY KEY (id));";
-    private final String DROPTABLEUSER = "DROP TABLE IF EXISTS userdb.user";
-    private final String SAVEUSER = "INSERT INTO userdb.user (name, lastname, age) VALUES(?,?,?)";
-    private final String DELETEUSERBYID = "DELETE FROM userdb.user WHERE id = ?";
-    private final String GETALLUSERS = "SELECT * FROM userdb.user";
-    private final String CLEANUSERSTABLE = "TRUNCATE TABLE userdb.user";
-
-    public UserDaoJDBCImpl() {
-        con = Util.createDBConnection();
-    }
-
+    @Override
     public void createUsersTable() {
-        try {
-            PreparedStatement pstm = con.prepareStatement(CREATEUSERTABLE);
+        String create_user_table = "CREATE TABLE IF NOT EXISTS `userdb`.`user` (\n" +
+                "  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NULL,\n" +
+                "  `last_name` VARCHAR(45) NULL,\n" +
+                "  `age` TINYINT(3) NULL,\n" +
+                "  PRIMARY KEY (id));";
+        try (Connection con = Util.createDBConnection()) {
+            PreparedStatement pstm = con.prepareStatement(create_user_table);
             pstm.execute();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
 
+    @Override
     public void dropUsersTable() {
-        try {
-            PreparedStatement pstm = con.prepareStatement(DROPTABLEUSER);
+        String drop_table_user = "DROP TABLE IF EXISTS userdb.user";
+        try (Connection con = Util.createDBConnection()) {
+            PreparedStatement pstm = con.prepareStatement(drop_table_user);
             pstm.execute();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            PreparedStatement pstm = con.prepareStatement(SAVEUSER);
+        String save_user = "INSERT INTO userdb.user (name, last_name, age) VALUES(?,?,?)";
+        try (Connection con = Util.createDBConnection()) {
+            PreparedStatement pstm = con.prepareStatement(save_user);
             pstm.setString(1, name);
             pstm.setString(2, lastName);
             pstm.setByte(3, age);
             pstm.executeUpdate();
             System.out.printf(" User c именем - %s добавлен в базу данных \n", name);
         } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
+    @Override
     public void removeUserById(long id) {
-        try {
-            PreparedStatement pstm = con.prepareStatement(DELETEUSERBYID);
+        String delete_user_byid = "DELETE FROM userdb.user WHERE id = ?";
+        try (Connection con = Util.createDBConnection()) {
+            PreparedStatement pstm = con.prepareStatement(delete_user_byid);
             pstm.setLong(1, id);
             pstm.executeUpdate();
         } catch (SQLException ex) {
@@ -67,26 +67,30 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
+    @Override
     public List<User> getAllUsers() {
+        String get_all_users = "SELECT * FROM userdb.user";
         List<User> users = new ArrayList<>();
-        try {
-            PreparedStatement pstm = con.prepareStatement(GETALLUSERS);
+        try (Connection con = Util.createDBConnection()) {
+            PreparedStatement pstm = con.prepareStatement(get_all_users);
             ResultSet resultSet = pstm.executeQuery();
             while (resultSet.next()) {
                 users.add(new User(resultSet.getLong(1), resultSet.getString(2), resultSet.getString(3),
                         resultSet.getByte(4)));
             }
+            users.forEach(System.out::println);
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        users.stream().forEach(System.out::println);
         return users;
     }
 
+    @Override
     public void cleanUsersTable() {
-        try {
+        String clean_user_table = "TRUNCATE TABLE userdb.user";
+        try (Connection con = Util.createDBConnection()) {
             Statement stm = con.createStatement();
-            stm.execute(CLEANUSERSTABLE);
+            stm.execute(clean_user_table);
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
