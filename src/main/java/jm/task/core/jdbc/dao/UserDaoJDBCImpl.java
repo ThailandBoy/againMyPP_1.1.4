ff5280e6e42422018a88c1699d3acf4e3209ccb5
ThailandBoy <91521829+ThailandBoy@users.noreply.github.com>
2023-06-25 20:27:22 +0300

    Исправил по конвенции.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -12,17 +12,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection con;
+    public UserDaoJDBCImpl() {
+        con = Util.createDBConnection();
+    }
 
     @Override
     public void createUsersTable() {
-        String create_user_table = "CREATE TABLE IF NOT EXISTS `userdb`.`user` (\n" +
+        String createUsersTable = "CREATE TABLE IF NOT EXISTS `userdb`.`user` (\n" +
                 "  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,\n" +
                 "  `name` VARCHAR(45) NULL,\n" +
                 "  `last_name` VARCHAR(45) NULL,\n" +
                 "  `age` TINYINT(3) NULL,\n" +
                 "  PRIMARY KEY (id));";
-        try (Connection con = Util.createDBConnection()) {
-            PreparedStatement pstm = con.prepareStatement(create_user_table);
+        try {
+            PreparedStatement pstm = con.prepareStatement(createUsersTable);
             pstm.execute();
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -31,9 +35,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        String drop_table_user = "DROP TABLE IF EXISTS userdb.user";
-        try (Connection con = Util.createDBConnection()) {
-            PreparedStatement pstm = con.prepareStatement(drop_table_user);
+        String dropUsersTable = "DROP TABLE IF EXISTS userdb.user";
+        try {
+            PreparedStatement pstm = con.prepareStatement(dropUsersTable);
             pstm.execute();
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -42,9 +46,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        String save_user = "INSERT INTO userdb.user (name, last_name, age) VALUES(?,?,?)";
-        try (Connection con = Util.createDBConnection()) {
-            PreparedStatement pstm = con.prepareStatement(save_user);
+        String saveUser = "INSERT INTO userdb.user (name, last_name, age) VALUES(?,?,?)";
+        try {
+            PreparedStatement pstm = con.prepareStatement(saveUser);
             pstm.setString(1, name);
             pstm.setString(2, lastName);
             pstm.setByte(3, age);
@@ -57,9 +61,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        String delete_user_byid = "DELETE FROM userdb.user WHERE id = ?";
-        try (Connection con = Util.createDBConnection()) {
-            PreparedStatement pstm = con.prepareStatement(delete_user_byid);
+        String deleteUserById = "DELETE FROM userdb.user WHERE id = ?";
+        try {
+            PreparedStatement pstm = con.prepareStatement(deleteUserById);
             pstm.setLong(1, id);
             pstm.executeUpdate();
         } catch (SQLException ex) {
@@ -69,10 +73,10 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        String get_all_users = "SELECT * FROM userdb.user";
+        String getAllUsers = "SELECT * FROM userdb.user";
         List<User> users = new ArrayList<>();
-        try (Connection con = Util.createDBConnection()) {
-            PreparedStatement pstm = con.prepareStatement(get_all_users);
+        try {
+            PreparedStatement pstm = con.prepareStatement(getAllUsers);
             ResultSet resultSet = pstm.executeQuery();
             while (resultSet.next()) {
                 users.add(new User(resultSet.getLong(1), resultSet.getString(2), resultSet.getString(3),
@@ -87,10 +91,10 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        String clean_user_table = "TRUNCATE TABLE userdb.user";
-        try (Connection con = Util.createDBConnection()) {
+        String cleanUsersTable = "TRUNCATE TABLE userdb.user";
+        try {
             Statement stm = con.createStatement();
-            stm.execute(clean_user_table);
+            stm.execute(cleanUsersTable);
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
